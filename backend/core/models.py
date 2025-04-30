cbf6fe49f3e0bbccf9b6635e050ba4bedea702e9
Samir Neupane <sasuki984@gmail.com>
2025-04-30 10:28:32 +0545

    fix: make email field uniqe

--- backend/core/models.py
+++ backend/core/models.py
@@ -14,6 +14,7 @@ from core.choices import StreamChoices
 class User(AbstractUser):
     phone_number = models.CharField(max_length=20, default="")
     location = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
 
     def __str__(self):
         return self.username
